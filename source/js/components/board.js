8527300c921cf4f1e3e302a3b23a7cec4e7018fb
kds <koutsenko@gmail.com>
2017-04-20 20:54:13 +0300

    Упрощена верстка components/board

--- source/js/components/board.js
+++ source/js/components/board.js
@@ -27,17 +27,21 @@ class Board extends React.Component {
   render() {
     var deckCards = this.stackCards.call(this, this.props.deck);
     var openCards = this.stackCards.call(this, this.props.open);
-    var home1Cards = this.stackCards.call(this, this.props.homes[0]);
-    var home2Cards = this.stackCards.call(this, this.props.homes[1]);
-    var home3Cards = this.stackCards.call(this, this.props.homes[2]);
-    var home4Cards = this.stackCards.call(this, this.props.homes[3]);
-    var stack1Cards = this.stackCards.call(this, this.props.stacks[0]);
-    var stack2Cards = this.stackCards.call(this, this.props.stacks[1]);
-    var stack3Cards = this.stackCards.call(this, this.props.stacks[2]);
-    var stack4Cards = this.stackCards.call(this, this.props.stacks[3]);
-    var stack5Cards = this.stackCards.call(this, this.props.stacks[4]);
-    var stack6Cards = this.stackCards.call(this, this.props.stacks[5]);
-    var stack7Cards = this.stackCards.call(this, this.props.stacks[6]);
+    var homesCards = [
+      this.stackCards.call(this, this.props.homes[0]),
+      this.stackCards.call(this, this.props.homes[1]),
+      this.stackCards.call(this, this.props.homes[2]),
+      this.stackCards.call(this, this.props.homes[3])
+    ];
+    var stacksCards = [
+      this.stackCards.call(this, this.props.stacks[0]),
+      this.stackCards.call(this, this.props.stacks[1]),
+      this.stackCards.call(this, this.props.stacks[2]),
+      this.stackCards.call(this, this.props.stacks[3]),
+      this.stackCards.call(this, this.props.stacks[4]),
+      this.stackCards.call(this, this.props.stacks[5]),
+      this.stackCards.call(this, this.props.stacks[6])
+    ];
 
     return (
       <div id="board">
@@ -49,41 +53,18 @@ class Board extends React.Component {
             {openCards}
           </Open>
           <Status />
-          <Home cardDropHandler={this.props.cardDrop} index={0}>
-            {home1Cards}
-          </Home>
-          <Home cardDropHandler={this.props.cardDrop} index={1}>
-            {home2Cards}
-          </Home>
-          <Home cardDropHandler={this.props.cardDrop} index={2}>
-            {home3Cards}
-          </Home>
-          <Home cardDropHandler={this.props.cardDrop} index={3}>
-            {home4Cards}
-          </Home>
+          {homesCards.map(function(home, index) {
+            return (
+              <Home key={index} cardDropHandler={this.props.cardDrop} index={index} children={home}/>
+            );
+          }.bind(this))}
         </div>
         <div className="row">
-          <Stack cardDropHandler={this.props.cardDrop} index={0}>
-            {stack1Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={1}>
-            {stack2Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={2}>
-            {stack3Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={3}>
-            {stack4Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={4}>
-            {stack5Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={5}>
-            {stack6Cards}
-          </Stack>
-          <Stack cardDropHandler={this.props.cardDrop} index={6}>
-            {stack7Cards}
-          </Stack>
+          {stacksCards.map(function(stack, index) {
+            return (
+              <Stack key={index} cardDropHandler={this.props.cardDrop} index={index} children={stack}/>
+            );
+          }.bind(this))}
         </div>
       </div>
     );
