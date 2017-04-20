d8bdcaa0e4be67678a6e623bf78c4db1090ea45d
kds <koutsenko@gmail.com>
2017-04-20 21:39:33 +0300

    Небольшой фикс анимаций - теперь визуально нажатие кнопки меню прослеживается всегда

--- source/js/controls/menu/button.js
+++ source/js/controls/menu/button.js
@@ -3,6 +3,13 @@ import React from 'react';
 import interact from 'interact.js';
 
 class MenuButton extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      pressed: false
+    }
+  }
+
   componentDidMount() {
     let ir = interact(this.refs[this.props.role]);
     ir.on(['tap'], this.handlePress.bind(this));
@@ -16,17 +23,20 @@ class MenuButton extends React.Component {
       return;
     }
 
-    let el = this.refs[this.props.role];
-    el.className += ' pressed';
+    this.setState({
+      pressed: true
+    });
     setTimeout(function() {
-        el.className = el.className.replace(' pressed', '');
-    }, 250);
+      this.setState({
+        pressed: false
+      });
+    }.bind(this), 250);
     this.props.handler();
   }
 
   render() {
     return (
-      <div title={this.props.hint} className={this.props.role + " button" + (this.props.disabled ? ' disabled' : '')} ref={this.props.role}>
+      <div title={this.props.hint} className={this.props.role + " button" + (this.props.disabled ? ' disabled' : '') + (this.state.pressed ? ' pressed' : '')} ref={this.props.role}>
         <div>{this.props.text}</div>
       </div>
     );
