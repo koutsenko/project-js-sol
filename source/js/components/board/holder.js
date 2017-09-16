fa5206e1b035fef3c213b25933c9ab7f643a87f2
kds <koutsenko@gmail.com>
2017-09-16 18:44:10 +0300

    Чистка <Holder>: перенос .declined в CSS, удален ReactMotion

--- source/js/components/board/holder.js
+++ source/js/components/board/holder.js
@@ -1,42 +1,25 @@
 import   React                from 'react'                            ;
 import { bindActionCreators } from 'redux'                            ;
 import { connect }            from 'react-redux'                      ;
-import { Motion, spring }     from 'react-motion'                     ;
 
 import   boardActions         from 'actions/board'                    ;
 import   constantsBoard       from 'constants/board'                  ;
 
 class Holder extends React.PureComponent {
-  getRef(element) {
-    this.Ref = element;
-  }
-
   render() {
+    let className = this.props.className;
+    if (this.props.declined) {
+      className += ' declined';
+    } else {
+      if (this.props.hovered === constantsBoard.highlights.ACCEPT) {
+        className += ' hovered yes';
+      } else if (this.props.hovered === constantsBoard.highlights.DENY) {
+        className += ' hovered no';
+      }
+    }
+
     return (
-      <Motion defaultStyle={{
-        dColor: +!this.props.declined
-      }} style={{
-        dColor: spring(+!this.props.declined)
-      }}>
-        {
-          function(interpolatingStyle) {
-            let style = this.props.declined ? {
-              boxShadow: `0 0 0.1em 0.3em rgba(255, 0, 0, ${interpolatingStyle.dColor})`
-            } : {};
-
-            let className = this.props.className;
-            if (this.props.hovered === constantsBoard.highlights.ACCEPT) {
-              className += ' hovered yes';
-            } else if (this.props.hovered === constantsBoard.highlights.DENY) {
-              className += ' hovered no';
-            }
-
-            return (
-              <div ref={this.getRef.bind(this)} style={style} data-index={this.props.index} data-id={this.props.id} className={className}/>
-            );
-          }.bind(this)
-        }
-      </Motion>
+      <div data-id={this.props.id} className={className}/>
     );
   }
 };
@@ -45,8 +28,7 @@ Holder.propTypes = {
   id              : React.PropTypes.string.isRequired,
   className       : React.PropTypes.string.isRequired,
   declined        : React.PropTypes.bool.isRequired,
-  index           : React.PropTypes.number,
-  cbFlushDeclined : React.PropTypes.func
+  hovered         : React.PropTypes.string
 };
 
 export default Holder;
